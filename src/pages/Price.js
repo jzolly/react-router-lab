9f918c41ca532c98cd8d46e8f8ef430768f54ee9
Jessie Zollinger <jzolly@hotmail.com>
2022-06-13 08:53:37 -0400

    all but price

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,4 +1,13 @@
-const Price = (props) => {
+import { useState, useEffect } from 'react';
+
+const Price = ({match}) => {
+    const symbol = match.params.symbol;
+    const [company, setCompany] = useState (null);
+    // const getCompany = async () => {
+    //     const response = await fetch
+    //      response.json();
+    //     setCompany(data);
+    // };
     return (
         <div>
             <h3>Name:</h3>
