d9831adbb50ced9b1c7a37154b7d98b757d53679
Jessie Zollinger <jzolly@hotmail.com>
2022-06-13 09:15:39 -0400

    still stuck, will figure out

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
+import stocks from '../data';
 
 const Price = ({match}) => {
-    const symbol = match.params.symbol;
-    const [company, setCompany] = useState (null);
+    const symbol = match.params.symbol.filter;
+    // const [company, setCompany] = useState (null);
     // const getCompany = async () => {
     //     const response = await fetch
     //      response.json();
@@ -10,8 +11,8 @@ const Price = ({match}) => {
     // };
     return (
         <div>
-            <h3>Name:</h3>
-            <h3>Price:</h3>
+            <h3>Name: {stocks.name}</h3>
+            <h3>Price: {stocks.lastPrice}</h3>
         </div>
     );
 };
